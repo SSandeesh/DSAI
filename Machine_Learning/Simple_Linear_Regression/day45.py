f13905d5e3126d5865d49b5288f46726eb16ebcf
SSandeesh <sandeesh120@gmail.com>
2025-07-08 10:32:46 +0530

    Simple Linear Regression

---
+++ Machine_Learning/Simple_Linear_Regression/day45.py
@@ -0,0 +1,32 @@
+ # day45
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+salarydf=pd.read_csv(r"C:\Users\sande\Downloads\Salary_Data.csv")
+
+x=salarydf.iloc[:,:1]
+y=salarydf.iloc[:,1] 
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20,random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+y_pred=regressor.predict(x_test)
+
+# Compare predicted and actual salaries from test set
+comparison= pd.DataFrame({"Actual":y_test,"Predicted":y_pred})
+print(comparison)
+
+# Visualize the test set
+plt.scatter(x_test,y_test,color="red")
+plt.plot(x_train,regressor.predict(x_train),color="blue")
+plt.title("Salary VS Experience(Test Set)",c="r")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
